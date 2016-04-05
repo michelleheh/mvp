57885445e342f090559af996e1af307676fbf23a
michelleheh <xiaoyu.he22@gmail.com>
2016-04-05 15:00:53 -0700

    add notes data

--- client/data/data.js
+++ client/data/data.js
@@ -24,84 +24,16 @@ data.projects = [
   },
 ];
 
-data.tasks = [{
-    task: "Jean Nicoli",
-    description: "Margonajo",
-    startTime: "2013-1-9 12:10", //year/month/day
-    endTime: "2013-1-10 18:24",
-    details: "120 vl, 30 er, 350 pax, md "
+data.notes = [
+  {
+    title: "Practice Piano",
+    text: "Step 1: Getting Familiar with Your Notes. ...\n Step 2: Getting Familiar with the Piano Keys. ...",
+    url: "https://i.ytimg.com/vi/4gka7wq0-Ss/maxresdefault.jpg"
   },
-
   {
-    task: "Mega Express III",
-    description: "Trois Maries N",
-    startTime: "2013-1-9 8:24",
-    endTime: "2013-1-9 18:24",
-    details: "110 vl, 250 pax, md cl3"
+    title: "Best Dog food",
+    text: "The following lists of best dog foods are grouped by category. \n Each includes a brief explanation of how we made our selections.",
+    url: "http://www.safestkitts.org/uploads/1/4/1/0/14108477/s986986427336998704_p5_i2_w347.jpeg"
   },
-
-  {
-    task: "Mega Express II",
-    description: "Trois Maries S",
-    startTime: "2013-1-6 0:0",
-    endTime: "2013-1-9 10:10"
-  },
-
-  {
-    task: "Girolata",
-    description: "Capucins N",
-    startTime: "2013-1-2 11:20",
-    endTime: "2013-1-3 18:06",
-    details: "48 ER"
-  },
-
-  {
-    task: "Surcouf",
-    description: "Capucins S",
-    startTime: "2013-1-6 14:25",
-    endTime: "2013-1-9 14:35"
-  },
-
-  {
-    task: "Queen Elisabeth",
-    description: "Mole Croisières",
-    startTime: "2013-1-9 6:53",
-    endTime: "2013-1-12 7:24",
-    details: "2350 pax"
-  },
-
-  {
-    task: "Vega",
-    description: "Mole Croisières",
-    startTime: "2013-1-12 00:24",
-    endTime: "2013-1-14 14:04",
-    details: "Inaugurale"
-  }, {
-    task: "East Indian Ocean",
-    description: "M Croisières",
-    startTime: "2013-1-8 1:45",
-    endTime: "2013-1-13 3:00",
-    details: "L 320 m, l 58 m"
-  },
-
-  {
-    task: "crying",
-    description: "Jeanne d'Arc",
-    startTime: "2013-1-13 06:00",
-    endTime: "2013-1-16 07:00"
-  },
-
-  {
-    task: "crying II",
-    description: "St Joseph",
-    startTime: "2013-1-11 14:35",
-    endTime: "2013-1-16 21:00"
-  }, 
-  {
-    task: "Belouga",
-    description: "Mouillage 1",
-    startTime: "2013-1-7 8:12",
-    endTime: "2013-1-14 9:30"
-  }
-
+  
 ];
